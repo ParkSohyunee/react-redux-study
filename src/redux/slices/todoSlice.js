9cdfd868f9e8f89dc97a9ec2a83a1f381d7ed637
ParkSohyunee <sohyunee016@gmail.com>
2023-11-29 22:47:09 +0900

    study - Reducer 내에서 현재 state 출력하기(current 함수)

--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = [];
 
@@ -13,12 +13,17 @@ const todoSlice = createSlice({
      * draft state를 자유롭게 수정하면 됨!!
      */
     addTodo: (state, action) => {
+      // console.log(state); // Proxy(Array) {0: {…}} => Proxy 객체가 출력됨!!
+      console.log(current(state)); // []
       state.push(action.payload); // return 기존배열에 push
+      console.log(current(state)); // ['todo']
     },
     removeTodo: (state, action) => {
+      console.log(current(state));
       return state.slice(0, -1); // return 기존배열을 slice
     },
     removeAll: (state, action) => {
+      console.log(current(state)); // 삭제 직전 값이 출력
       return initialState; // 빈 배열을 return하면서 reset
     },
   },
